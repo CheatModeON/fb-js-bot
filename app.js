029488ff4a3c3f4035e0ca59248c7c451657b58b
Achilles <achipap@hotmail.com>
2020-03-14 02:32:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ if (!process.env.VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY) {
 
 // Creating the bot with access token, name and avatar
 const bot = new ViberBot(logger, {
-    authToken: process.env.VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY, // Learn how to get your access token at developers.viber.com
-    name: "Is It Up",
+    authToken: "4b352455f127dc7d-abf771323d3e435f-95efbc9daeaf7b85", 
+    name: "Coronavirus Live News",
     avatar: "https://raw.githubusercontent.com/devrelv/drop/master/151-icon.png" // Just a placeholder avatar to display the user
 });
 
