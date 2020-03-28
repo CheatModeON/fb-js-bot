ec35ab97e45bfd79c6f42935938f03fe1307364b
Achilles <achipap@hotmail.com>
2020-03-28 18:21:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,4 +80,4 @@ bot.onTextMessage(/./, (message, response) => {
     checkUrlAvailability(response, message.text);
 });
 
-http.createServer(bot.middleware()).listen(5000, () => bot.setWebhook("viber-js-bot.azurewebsites.net" || "viber-js-bot.scm.azurewebsites.net:443"));
+http.createServer(bot.middleware()).listen(5000, () => bot.setWebhook("https://viber-js-bot.azurewebsites.net"));
